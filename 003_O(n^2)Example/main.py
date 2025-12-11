224770534a36a102d49a39043fa61ee7f0dc3ac1
Igor Pieper <91740800+IgorPieper@users.noreply.github.com>
2025-12-11 01:06:33 +0100

    Add function to log all pairs with O(n^2) complexity
    Implement a function to log all pairs of elements in an array.

---
+++ 003_O(n^2)Example/main.py
@@ -0,0 +1,10 @@
+boxes = ['a', 'b', 'c', 'd', 'e']
+
+def log_all_pairs(array):
+    for i in range(len(array)):
+        for j in range(len(array)):
+            print(array[i], array[j])
+
+log_all_pairs(boxes)
+
+# O(n^2) time complexity
