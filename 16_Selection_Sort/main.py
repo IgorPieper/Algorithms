3f74320045c682015719f4495a8bf00dca518e72
Igor Pieper <91740800+IgorPieper@users.noreply.github.com>
2025-12-13 07:07:42 +0100

    Add selection sort implementation in main.py
    Implement selection sort algorithm to sort an array.

---
+++ 16_Selection_Sort/main.py
@@ -0,0 +1,18 @@
+# Time Complexity: O(n^2)
+def selection_sort(arr):
+    n = len(arr)
+
+    for i in range(n):
+        min_index = i
+
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+    return arr
+
+
+numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
+print(selection_sort(numbers))
