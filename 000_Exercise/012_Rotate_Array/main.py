13cdaa6862e1f3b510b3d857bd03ea2bd4dbe549
Igor Pieper <91740800+IgorPieper@users.noreply.github.com>
2025-12-16 20:54:51 +0100

    Implement array rotation function

---
+++ 000_Exercise/012_Rotate_Array/main.py
@@ -0,0 +1,10 @@
+def rotate_array(nums, k):
+    n = len(nums)
+    k = k % n  # In case k is greater than n
+    return nums[-k:] + nums[:-k]
+
+nums = [1,2,3,4,5,6,7]
+k = 3
+
+rotated = rotate_array(nums, k)
+print(rotated)
